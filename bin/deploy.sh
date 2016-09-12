0a06f7235206057f39340c4d06c9eabd8ff59963
Fabian Keller <hello@fabian-keller.de>
2016-09-12 19:19:33 +0200

    Update: simplify build and deployment script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,6 +2,7 @@
 
 echo "Deploying to: $DEPLOY_REMOTE"
 
+cd target/mvn-repo/
 rsync --checksum \
     --ignore-times \
     --human-readable \
@@ -14,5 +15,5 @@ rsync --checksum \
     --executability \
     --delay-updates \
     --compress --skip-compress=gz/zip/z/rpm/deb/iso/bz2/t[gb]z/7z/mp[34]/mov/avi/ogg/jpg/jpeg/png/gif \
-    target/mvn-repo/ \
+    ./ \
     $DEPLOY_REMOTE
